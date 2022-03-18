4a88d80074af30909bb3b13385c6f3eddec709a7
Andrew Timmons <Andrew.Timmons@4rhuskies.org>
2022-03-18 09:56:02 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ export class AppComponent {
   public appPages = [
     { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
     { title: 'Madlib', url: '/folder/madlib/madlib', icon: 'newspaper'},
-    { title: 'Average Score/hw1-10', url: '/folder/hw1_10/hw1-prob10'},
-    { title: 'Favorite City/hw1-12', url: '/folder/hw1_12/hw1prob12'},
-    { title: 'Stocks/hw1-17', url: '/folder/hw1_17/hw1prob17'}
+    { title: 'Average Score/hw1-10', url: '/folder/hw1_10/hw1-prob10', icon: 'newspaper'},
+    { title: 'Favorite City/hw1-12', url: '/folder/hw1_12/hw1prob12', icon: 'bookmark'},
+    { title: 'Stocks/hw1-17', url: '/folder/hw1_17/hw1prob17', icon: 'cash'}
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
